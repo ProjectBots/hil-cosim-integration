e9a7f3cb0376c0c915d6a6fc5dc014efd791a669
ProjectBots <real.projectbots@gmail.com>
2025-09-18 12:03:20 +0200

    Add initial project files including .gitignore, .python-version, launch configuration, basic simulator code, and dependency management

---
+++ main.py
@@ -0,0 +1,62 @@
+import mosaik_api_v3
+
+import mosaik
+
+META = {
+    "type": "time-based",
+    "models": {
+        "MyModel": {
+            "public": True,
+            "params": ["p1", "p2"],
+            "attrs": ["a", "b"],
+        },
+    },
+}
+
+
+class MySimulator(mosaik_api_v3.Simulator):
+    def __init__(self):
+        super().__init__(META)
+        self.entities = {}
+
+    def init(self, sid, time_resolution, **sim_params):
+        print(f"Init called with sid={sid}, time_resolution={time_resolution}")
+        return self.meta
+
+    def create(self, num, model, **model_params):
+        entities = []
+        for i in range(num):
+            eid = f"{model}_{len(self.entities)}"
+            self.entities[eid] = model_params
+            entities.append({"eid": eid, "type": model})
+        return entities
+
+    def step(self, time, inputs, max_advance):
+        return time + 1
+
+    def get_data(self, outputs):
+        data = {}
+        for eid, attrs in outputs.items():
+            data[eid] = {a: 42 for a in attrs}  # dummy values
+        return data
+
+
+sim_config: mosaik.SimConfig = {
+    "MySim": {
+        "python": "main:MySimulator",
+    }
+}
+
+
+def main():
+    world = mosaik.World(sim_config)
+
+    mysim = world.start("MySim", sim_param1=123)
+
+    a = mysim.MyModel.create(1, p1=10)
+
+    world.run(until=100)
+
+
+if __name__ == "__main__":
+    main()
