65fd0579748b54cb945a6ee3fd8257b4e66c0702
ProjectBots <real.projectbots@gmail.com>
2025-11-17 17:07:13 +0100

    set rt_factor to 1.0 for real-time simulations

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import scenario.scenariofactory as scenariofactory
 
 IS_REALTIME = True
 
-STEP_SIZE_SECONDS = 60*60
+STEP_SIZE_SECONDS = 60 * 60
 STEPS_TOTAL = 500
 
 sim_config: mosaik.SimConfig = {
     "BatterySim": {
-        "python": "simulators.batteryreal:BatteryModel"  # simulators.batterysim:BatteryModelRT
+        "python": "simulators.batteryreal:BatteryModel"
         if IS_REALTIME
         else "simulators.batterysim:BatteryModel",
     },
@@ -28,7 +28,7 @@ sim_config: mosaik.SimConfig = {
         "python": "simulators.controllersim:BatteryControllerSim",
     },
     "WebVis": {
-        'cmd': 'mosaik-web -s 127.0.0.1:8000 %(addr)s',
+        "cmd": "mosaik-web -s 127.0.0.1:8000 %(addr)s",
     },
     "DebugSim": {
         "python": "simulators.debugsim:DebugSim",
@@ -42,13 +42,9 @@ def main():
     scenariofactory.add_simple_scenario(world, STEP_SIZE_SECONDS)
 
     # TODO: figure out how to get rid of behind schedule warnings when rt_factor is set
-    # world.run(
-    #     until=STEPS_TOTAL * STEP_SIZE_SECONDS, rt_factor=1.0 if IS_REALTIME else None
-    # )
-    world.run(until=STEPS_TOTAL * STEP_SIZE_SECONDS)    # no rt_factor for now to avoid warnings
-
-
-    
+    world.run(
+        until=STEPS_TOTAL * STEP_SIZE_SECONDS, rt_factor=1.0 if IS_REALTIME else None
+    )
 
 
 if __name__ == "__main__":
